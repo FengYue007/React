87f611b46d82eed37ce87281357d7ad041dfde3a
xiao-tang <10761101@gm.nfu.edu.tw>
2021-05-04 11:59:05 +0800

    first hello, world finish

---
+++ 02-study/01_helloworld/app.js
@@ -0,0 +1,16 @@
+// TODO: 1 start
+ReactDOM.render(
+  <div>
+    <h1>Hello, world!!</h1>
+    <h2>milkmidi</h2>
+  </div>,
+  document.getElementById('app')
+);
+
+/*
+ReactDOM.render(
+  <h1>Hello, world!!</h1>,
+  document.getElementById('app')
+);
+// */
+// TODO: 1 end
