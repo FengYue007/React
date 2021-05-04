4cfabd53f98d9f52652b1bf7d7823a381d716be6
xiao-tang <10761101@gm.nfu.edu.tw>
2021-05-04 16:37:46 +0800

    list-rendering-advance finish

--- 02-study/09_list-rendering-advance/app.js
+++ 02-study/09_list-rendering-advance/app.js
@@ -9,10 +9,55 @@ function CategoryItem(props){
     </div>
   )
   // */
+  return (
+    <div className="category-item">
+      <img src={props.image} />
+      <p>{props.name}</p>
+    </div>
+  )
 }
 
-function List(){
-  const [list, setList] = React.useState([]);
+// function List(){
+//   const [list, setList] = React.useState([]);
+//   React.useEffect(() => {
+//     const init = async() => {
+//       const rest = await fetch('./categories.json');
+//       const rest2 = await rest.json();
+//       setList(rest2)
+//     }
+//     init();
+//   }, []);
+React.useEffect(() => {
+  fetch('./categories.json')
+    .then((res) => res.json())
+    .then((categories) => {
+      console.log(categories);
+      setList(categories);
+    })
+}, []);
+
+React.useEffect(() => {
+  fetch('./')
+}
+
+return (
+  <div>
+    <div className="category-wrap">
+    {
+      list.map((category)=> {
+        return (
+          <CategoryItem 
+            key={category.id}
+            name={category.name}
+            image={category.image} />
+        )
+      })
+    }
+    </div>
+  </div>
+)
+
+// */
 
   /* // TODO2
   React.useEffect(() => {
@@ -23,6 +68,10 @@ function List(){
         setList(categories);
       })
   }, []);
+
+  React.useEffect(() => {
+    fetch('./')
+  }
 
   return (
     <div>
